4351183c2b5257101cbbe2c12ad2da6f9d235101
Mourad RL CADI <mouradface1998@gmail.com>
2022-12-28 21:47:14 +0100

    save

--- setupTests.ts
+++ setupTests.ts
@@ -1,6 +1,14 @@
 import "@testing-library/jest-dom";
-import { server } from "./src/mocks/server";
 
-beforeAll(() => server.listen());
+import { server } from "./src/mocks/server";
+import { fetch } from "cross-fetch";
+/* 
+    Polyfilling fetch, fetch will run indifrent environments node, browser ...
+    so run `npm install --save-dev cross-fetch`
+*/
+
+global.fetch = fetch;
+
+beforeAll(() => server.listen({ onUnhandledRequest: `error` }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
--- src/components/__tests__/http-mock.spec.tsx
+++ src/components/__tests__/http-mock.spec.tsx
@@ -1,6 +1,8 @@
 import Users from "@/http-mock";
 import { render, screen } from "~/../test-utils";
 import { users } from "~/mocks/handlers";
+import { server } from "~/mocks/server";
+import { rest } from "msw";
 
 // https://testing-library.com/docs/react-testing-library/example-intro/#mock
 
@@ -17,8 +19,20 @@ describe("Users", () => {
 
         const usersList = await screen.findAllByRole("listitem");
 
-        screen.debug();
-
         expect(usersList).toHaveLength(users.length);
+    });
+
+    test("error message appears when errors occur", async () => {
+        server.use(
+            // override the initial "GET /greeting" request handler
+            // to return a 500 Server Error
+            rest.get("https://jsonplaceholder.typicode.com/users", (_, res, ctx) => res(ctx.status(500)))
+        );
+
+        render(<Users />);
+
+        const errorEl = await screen.findByText(/went wrong/i);
+
+        expect(errorEl).toBeInTheDocument();
     });
 });
